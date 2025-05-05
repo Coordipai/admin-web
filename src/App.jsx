425a121d38efc9f48d1a207880ae89f873439ea2
gonu <90233723+makisepel@users.noreply.github.com>
2025-05-05 22:26:23 +0900

    feat: Add IssueDetailPage (#17)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { BuildProject } from './pages/build_project/BuildProject'
-import { BuildProject2 } from './pages/build_project/BuildProject2' 
-import { BuildProject3 } from './pages/build_project/BuildProject3'
-import { Project } from './pages/project/Project'
+import { BuildProject } from '@pages/build_project/BuildProject'
+import { BuildProject2 } from '@pages/build_project/BuildProject2' 
+import { BuildProject3 } from '@pages/build_project/BuildProject3'
+import { Project } from '@pages/project/Project'
+import IssueDetailPage from '@pages/issue/IssueDetailPage'
 import { Home } from './pages/Home'
 import ComponentTest from '@pages/ComponentTest'
 import { SettingProject } from './pages/build_project/SettingProject'
@@ -17,6 +18,7 @@ function App () {
         <Route path="/buildproject2" element={<BuildProject2/>}/>
         <Route path="/buildproject3" element={<BuildProject3/>}/>
         <Route path="/project" element={<Project/>}/>
+        <Route path="/issue" element={<IssueDetailPage/>}/>
         <Route path="/settingproject" element={<SettingProject/>}/>
       </Routes>
     </BrowserRouter>
