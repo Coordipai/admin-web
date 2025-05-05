4540aacd3e609929c5cb835df0fd3ea857c97ed6
조재용 <66457807+FriOct@users.noreply.github.com>
2025-05-05 10:43:08 +0900

    feat: Add home page (#11)

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { BuildProject } from './pages/build_project/BuildProject'
 import { BuildProject2 } from './pages/build_project/BuildProject2' 
 import { BuildProject3 } from './pages/build_project/BuildProject3'
 import { Project } from './pages/project/Project'
-
+import { Home } from './pages/Home'
 import ComponentTest from '@pages/ComponentTest'
 
 function App () {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<h1>hello world</h1>} />
+        <Route path='/' element={<Home />} />
         <Route path='components' element={<ComponentTest />} />
         <Route path="/buildproject" element={<BuildProject/>}/>
         <Route path="/buildproject2" element={<BuildProject2/>}/>
