91e3dfbbfc2f1796d1d649c44fe7479bde45aa2b
gonu <90233723+makisepel@users.noreply.github.com>
2025-05-11 16:22:03 +0900

    chore: Change api functions to use try-catch

--- src/api/issueApi.js
+++ src/api/issueApi.js
@@ -4,10 +4,15 @@ import { api } from '@hooks/useAxios'
  * 1. 모든 이슈 받아오기 (프로젝트별)
  * @param {number} projectId
  */
-export const fetchIssues = (projectId) => {
-  return api.get('/issue/', {
-    params: { project_id: projectId }
-  })
+export const fetchAllIssues = async (projectId) => {
+  try {
+    const response = await api.get('/issue/', { params: { project_id: projectId } });
+    return response.data;
+  } catch (error) {
+    console.error('[getAllIssues] Error:', error);
+    // ToastMessage 활용
+    throw error;
+  }
 }
 
 /**
@@ -15,35 +20,63 @@ export const fetchIssues = (projectId) => {
  * @param {number} projectId
  * @param {number} issueNumber
  */
-export const fetchIssueDetail = (projectId, issueNumber) => {
-  return api.get('/issue/detail', {
-    params: {
-      project_id: projectId,
-      issue_number: issueNumber
-    }
-  })
+export const fetchIssueDetail = async (projectId, issueNumber) => {
+  try {
+    const response = await api.get('/issue/detail', {
+      params: {
+        project_id: projectId,
+        issue_number: issueNumber
+      }
+    })
+    return response.data;
+  } catch (error) {
+    console.error('[getIssueDetail] Error:', error);
+    // ToastMessage 활용
+    throw error;
+  }
 }
 
 /**
  * 3. 이슈 추가하기
  * @param {object} issueData
  */
-export const createIssue = (issueData) => {
-  return api.post('/issue/', issueData)
+export const createIssue = async (issueData) => {
+  try {
+    const response = await api.post('/issue/', issueData)
+    return response.data
+  } catch (error) {
+    console.error('[createIssue] Error:', error)
+    // ToastMessage 활용
+    throw error
+  }
 }
 
 /**
  * 4. 이슈 수정하기
  * @param {object} issueData
  */
-export const updateIssue = (issueData) => {
-  return api.put('/issue/', issueData)
+export const updateIssue = async (issueData) => {
+    try {
+        const response = await api.patch('/issue/', issueData)
+        return response.data
+    } catch (error) {
+        console.error('[updateIssue] Error:', error)
+        // ToastMessage 활용
+        throw error
+    }
 }
 
 /**
  * 5. 이슈 닫기 (삭제로 간주)
  * @param {object} closeData { project_id, issue_number }
  */
-export const closeIssue = (closeData) => {
-  return api.patch('/issue/', closeData)
+export const closeIssue = async (closeData) => {
+  try {
+    const response = await api.delete('/issue/', { data: closeData })
+    return response.data
+  } catch (error) {
+    console.error('[closeIssue] Error:', error)
+    // ToastMessage 활용
+    throw error
+  }
 }
