6f2ad8ac71c3e1692ecee3adb95d41270098edf2
gonu <90233723+makisepel@users.noreply.github.com>
2025-05-11 17:06:18 +0900

    feat: Add toast message in issue api functions

--- src/api/issueApi.js
+++ src/api/issueApi.js
@@ -1,4 +1,5 @@
 import { api } from '@hooks/useAxios'
+import { showSuccessToastMsg, showErrorToastMsg } from '@utils/showToastMsg';
 
 /**
  * 1. 모든 이슈 받아오기 (프로젝트별)
@@ -7,10 +8,11 @@ import { api } from '@hooks/useAxios'
 export const fetchAllIssues = async (projectId) => {
   try {
     const response = await api.get('/issue/', { params: { project_id: projectId } });
+    console.log('[getAllIssues] Response:', response.data);
     return response.data;
   } catch (error) {
     console.error('[getAllIssues] Error:', error);
-    // ToastMessage 활용
+    showErrorToastMsg(error);
     throw error;
   }
 }
@@ -28,10 +30,11 @@ export const fetchIssueDetail = async (projectId, issueNumber) => {
         issue_number: issueNumber
       }
     })
+    console.log('[getIssueDetail] Response:', response.data);
     return response.data;
   } catch (error) {
     console.error('[getIssueDetail] Error:', error);
-    // ToastMessage 활용
+    showErrorToastMsg(error);
     throw error;
   }
 }
@@ -43,10 +46,11 @@ export const fetchIssueDetail = async (projectId, issueNumber) => {
 export const createIssue = async (issueData) => {
   try {
     const response = await api.post('/issue/', issueData)
+    showSuccessToastMsg('성공적으로 이슈가 생성되었습니다.')
     return response.data
   } catch (error) {
     console.error('[createIssue] Error:', error)
-    // ToastMessage 활용
+    showErrorToastMsg(error);
     throw error
   }
 }
@@ -58,10 +62,11 @@ export const createIssue = async (issueData) => {
 export const updateIssue = async (issueData) => {
     try {
         const response = await api.patch('/issue/', issueData)
+        showSuccessToastMsg('성공적으로 이슈가 수정되었습니다.')
         return response.data
     } catch (error) {
         console.error('[updateIssue] Error:', error)
-        // ToastMessage 활용
+        showErrorToastMsg(error);
         throw error
     }
 }
@@ -73,10 +78,11 @@ export const updateIssue = async (issueData) => {
 export const deleteIssue = async (closeData) => {
   try {
     const response = await api.delete('/issue/', { data: closeData })
+    showSuccessToastMsg('성공적으로 이슈가 닫혔습니다.')
     return response.data
   } catch (error) {
     console.error('[closeIssue] Error:', error)
-    // ToastMessage 활용
+    showErrorToastMsg(error);
     throw error
   }
 }
